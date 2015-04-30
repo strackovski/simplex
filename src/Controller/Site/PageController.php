0a944d2652ba792c4cf85c82f88c4fd08394bee8
strackovski <vlado@nv3.org>
2015-04-30 14:55:33 +0200

    Return single result when appropriate

--- src/Controller/Site/PageController.php
+++ src/Controller/Site/PageController.php
@@ -42,14 +42,56 @@ class PageController
         }
 
         $settings = $app['repository.settings']->getPublicSettings();
+        $page = $app['repository.page']->findOneBy(array('slug' => 'home'));
+        $content = array();
 
-        return $app['twig']->render(
-            'site/'.$settings->getPublicTheme().'/views/index.html.twig',
-            array(
-                'settings' => $settings,
-                'menu' => $app['repository.page']->getMenuPages()
-            )
-        );
+        if ($page instanceof Page and $page->getQueries()) {
+            foreach ($page->getQueries() as $query) {
+                try{
+                    if (!is_null($query->getOutputVariable())) {
+
+                        if ($query->getLimitMax() == 1) {
+                            try {
+                                $content[$query->getOutputVariable()] =
+                                    $query->getManager()->buildQuery($app['orm.em'])->getOneOrNullResult();
+                            } catch (\Exception $e) {
+                                $content[$query->getOutputVariable()] = null;
+                            }
+                        } else {
+                            $content[$query->getOutputVariable()] = $query->getManager()->buildQuery($app['orm.em'])->getResult();
+                        }
+
+                    } else {
+                        if ($query->getLimitMax() == 1) {
+                            try {
+                                $content[$query->getOutputVariable()] =
+                                    $query->getManager()->buildQuery($app['orm.em'])->getOneOrNullResult();
+                            } catch (\Exception $e) {
+                                $content[$query->getOutputVariable()] = null;
+                            }
+                        } else {
+                            $content[] = $query->getManager()->buildQuery($app['orm.em'])->getResult();
+                        }
+                    }
+                } catch (QueryException $e) {
+                    $app['monolog']->addError(
+                        'Failed building query in Site\\PageController:viewAction: ' . $e->getMessage()
+                    );
+                }
+            }
+
+            return $app['twig']->render(
+                'site/'.$app['settings']->getPublicTheme().'/views/'.$page->getView().'.html.twig',
+                array(
+                    'content' => $content,
+                    'page' => $page,
+                    'settings' => $settings,
+                    'menu' => $app['repository.page']->getMenuPages()
+                )
+            );
+        }
+
+        return $app->abort(404, 'Page not found.');
     }
 
     /**
@@ -73,9 +115,29 @@ class PageController
             foreach ($page->getQueries() as $query) {
                 try{
                     if (!is_null($query->getOutputVariable())) {
-                        $content[$query->getOutputVariable()] = $query->getManager()->buildQuery($app['orm.em'])->getResult();
+
+                        if ($query->getLimitMax() == 1) {
+                            try {
+                                $content[$query->getOutputVariable()] =
+                                    $query->getManager()->buildQuery($app['orm.em'])->getOneOrNullResult();
+                            } catch (\Exception $e) {
+                                $content[$query->getOutputVariable()] = null;
+                            }
+                        } else {
+                            $content[$query->getOutputVariable()] = $query->getManager()->buildQuery($app['orm.em'])->getResult();
+                        }
+
                     } else {
-                        $content[] = $query->getManager()->buildQuery($app['orm.em'])->getResult();
+                        if ($query->getLimitMax() == 1) {
+                            try {
+                                $content[$query->getOutputVariable()] =
+                                    $query->getManager()->buildQuery($app['orm.em'])->getOneOrNullResult();
+                            } catch (\Exception $e) {
+                                $content[$query->getOutputVariable()] = null;
+                            }
+                        } else {
+                            $content[] = $query->getManager()->buildQuery($app['orm.em'])->getResult();
+                        }
                     }
                 } catch (QueryException $e) {
                     $app['monolog']->addError(
